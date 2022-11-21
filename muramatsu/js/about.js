175ccac1079f79d7f017a3ad259fcc35c2e69691
村松春佳 <>
2022-11-21 21:57:48 +0900

    1121

--- muramatsu/js/about.js
+++ muramatsu/js/about.js
@@ -56,10 +56,12 @@ scope.appendTo(target);
 $(function(){
     $(window).scroll(function (){
         $('.fadein').each(function(){
+          
             var elementTop = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
         if (scroll > elementTop - windowHeight + 100){
+
             $(this).addClass('scrollin');
         }
     });
--- muramatsu/js/script.js
+++ muramatsu/js/script.js
@@ -1,57 +1,20 @@
-//---------メインテキストここから---------//
-function RollAnimeControl() {
-    $('.rollAnime').each(function () {// rollAnimeにrollというクラス名を付ける定義
-        var elemPos = $(this).offset().top - 50;//移動先の位置を指定
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        var childs = $(this).children();  //rollAnimeの子要素を取得。今回の場合だと名前
-        if (scroll >= elemPos - windowHeight) {
-            $(childs).each(function (i) {   //子要素を1つ1つ処理をおこなう
-            if (i < 10) {         //10未満の場合
-            $(this).css("transition-delay","."+i+"s");  //子要素にcsstransition-delay(変化開始までの時間)を追加
-            }else {             //10以上の場合
-                var n = i / 10;       //ミリ秒指定なので10で割る
-                $(this).css("transition-delay",n+"s");  //子要素にcsstransition-delayを追加
-            }
-            });
-            $(this).addClass("roll"); //rollというアニメーションクラスを付与
-    
-        } else {
-            $(childs).each(function () {    //子要素を1つ1つ処理をおこなう
-            $(this).css("transition-delay","0s");//子要素にcsstransition-delayの秒を0とする
-            });
-            $(this).removeClass("roll");//rollというアニメーションクラスを除去
+//---------フェードインここから---------//
+$(function(){
+    $(window).scroll(function (){
+        $('.about-button-wrapper').each(function(){
+            console.log("123");
+            var elementTop = $(this).offset().top;//about-button-wrapper要素の位置取得
+            var scroll = $(window).scrollTop();//どのくらいスクロールされたか取得
+            var windowHeight = $(window).height();//ウィンドウの高さ取得
+        if (scroll > elementTop - windowHeight + 200){//がスクロール位置が(要素位置-ウィンドウ高さ+100)より大きければ
+            $(this).addClass('scroll_in');//クラス追加する
         }
-        });
-    }
-
-  // 画面が読み込まれたらすぐに動かしたい場合の記述
-    $(window).on('load', function () {
-        //spanタグを追加する
-        var element = $(".rollAnime");
-        element.each(function () {
-        var text = $(this).text();
-        var textbox = [];
-        text.split('').forEach(function (t, i) {
-            if (t !== " ") {
-            if (i < 10) {
-                textbox += '<span style="transition-delay:.' + i + 's;">' + t + '</span>';
-            } else {
-                var n = i / 10;
-                textbox += '<span style="transition-delay:' + n + 's;">' + t + '</span>';
-            }
-            } else {
-            textbox += t;
-            }
-        });
-        $(this).html(textbox);
-        });
-    
-        RollAnimeControl();/* アニメーション用の関数を呼ぶ*/
     });
+});
+});
 
 //---------カーソルここから---------//
-var cursor = document.getElementById('cursor'); 
+var cursor = document.getElementById('cursor');
 document.addEventListener('mousemove', function (e) {
     cursor.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
 });
@@ -62,62 +25,61 @@ for (var i = 0; i < link.length; i++) {
         cursor.classList.add('cursor--hover');
     });
     link[i].addEventListener('mouseout', function (e) {
-        cursor.classList.remove('cursor--hover');   
+        cursor.classList.remove('cursor--hover');
     });
 }
 
 //---------ローティングここから---------//
-const loading = document.querySelector( '.loading' );
-window.addEventListener( 'load', () => {
-    loading.classList.add( 'hide' );
-}, false );
+const loading = document.querySelector('.loading');
+window.addEventListener('load', () => {
+    loading.classList.add('hide');
+}, false);
 
 //---------スクロールここから---------//
-$(document).ready(function() {
+$(document).ready(function () {
     $('#pagepiling').pagepiling({
-    menu: '#menu',
-    anchors: ['page1', 'page2', 'page3'],
-    sectionsColor: ['#000', '#5c9291', '#000'],
-    navigation: {
-        'position': 'right',
-        'tooltips': ['緑色', '桃色', '黒色']
-    }
-});
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3'],
+        sectionsColor: ['#000', '#71A0A0', '#000'],
+        navigation: {
+            'position': 'right',
+            'tooltips': ['灰色', '緑色', '黒色']
+        }
+    });
 });
 
 //---------水滴ここから---------//
-    $('#section1,#section2').ripples({
-        resolution: 512,   //解像度
-        dropRadius: 20,    //ドロップサイズ
-        perturbance: 0.04, //屈折量
-    });
+$('#section1,#section2').ripples({
+    resolution: 512,   //解像度
+    dropRadius: 20,    //ドロップサイズ
+    perturbance: 0.04, //屈折量
+});
 
 //---------Contactスクロールここから---------//
 (function ($) {
     'use strict';
     let scrollPosi = 0;
     $(window).scroll(function () {
-    scrollPosi = $(document).scrollTop();
-    $('#para1').stop(true, true).animate({
-        'background-position-y': -scrollPosi / 3 + 'px'
-    }, 100);
-});
+        scrollPosi = $(document).scrollTop();
+        $('#para1').stop(true, true).animate({
+            'background-position-y': -scrollPosi / 3 + 'px'
+        }, 100);
+    });
 })
 
 //---------メインビジュアルテキストここから---------//
-// テキストの設定です。詳細なプロパティは公式のドキュメントにあります。
 let Maintext = new Blotter.Text("HaruKa Muramatsu", {
-    family : "serif",
-    size : 120,
-    fill : "#FFF",
+    family: "serif",
+    size: 120,
+    fill: "#FFF",
 });
 // 仕様するエフェクトを定義。
 let Mainmaterial = new Blotter.LiquidDistortMaterial();
-Mainmaterial.uniforms.uSpeed.value = 0.5;
+Mainmaterial.uniforms.uSpeed.value = 0.3;
 Mainmaterial.uniforms.uVolatility.value = 0.04;
 Mainmaterial.uniforms.uSeed.value = 6.02;
 // 設定したテキストとエフェクトをセット。
-let Mainblotter = new Blotter(Mainmaterial, { texts : Maintext });
+let Mainblotter = new Blotter(Mainmaterial, { texts: Maintext });
 // ほぼおまじない。
 let Mainscope = Mainblotter.forText(Maintext);
 // 任意のDOMを用意
@@ -125,11 +87,10 @@ let Maintarget = document.getElementById('data-blotter');
 Mainscope.appendTo(Maintarget);
 
 //---------Aboutテキストここから---------//
-// テキストの設定です。詳細なプロパティは公式のドキュメントにあります。
 let text = new Blotter.Text("About", {
-    family : "serif",
-    size : 80,
-    fill : "#FFF"
+    family: "serif",
+    size: 80,
+    fill: "#FFF"
 });
 // 仕様するエフェクトを定義。
 let material = new Blotter.LiquidDistortMaterial();
@@ -137,7 +98,7 @@ material.uniforms.uSpeed.value = 0.6;
 material.uniforms.uVolatility.value = 0.05;
 material.uniforms.uSeed.value = 6.02;
 // 設定したテキストとエフェクトをセット。
-let blotter = new Blotter(material, { texts : text });
+let blotter = new Blotter(material, { texts: text });
 // ほぼおまじない。
 let scope = blotter.forText(text);
 // 任意のDOMを用意
@@ -145,11 +106,10 @@ let target = document.getElementById('about-title');
 scope.appendTo(target);
 
 //---------Workテキストここから---------//
-// テキストの設定です。詳細なプロパティは公式のドキュメントにあります。
 let Worktext = new Blotter.Text("Works", {
-    family : "serif",
-    size : 80,
-    fill : "#FFF"
+    family: "serif",
+    size: 80,
+    fill: "#FFF"
 });
 // 仕様するエフェクトを定義。
 let Workmaterial = new Blotter.LiquidDistortMaterial();
@@ -157,9 +117,10 @@ Workmaterial.uniforms.uSpeed.value = 0.6;
 Workmaterial.uniforms.uVolatility.value = 0.05;
 Workmaterial.uniforms.uSeed.value = 6.02;
 // 設定したテキストとエフェクトをセット。
-let Workblotter = new Blotter(Workmaterial, { texts : Worktext });
+let Workblotter = new Blotter(Workmaterial, { texts: Worktext });
 // ほぼおまじない。
 let Workscope = Workblotter.forText(Worktext);
 // 任意のDOMを用意
 let Worktarget = document.getElementById('works-title');
 Workscope.appendTo(Worktarget);
+
